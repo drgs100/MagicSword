984c661ab150544456e4f0b04ff250cd4a9257f2
Damian Sefton <damian.sefton@nhs.net>
2020-07-31 16:26:10 +0100

    Add new Controller, Model and View

---
+++ MagicWeapon/Controllers/GreetingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using MagicWeapon.Models;
+
+namespace MagicWeapon.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET: Greeting
+        public ActionResult Index()
+        {
+            var model = new GreetingViewModel();
+            model.Message = ConfigurationManager.AppSettings["Message"];
+            return View(model);
+        }
+    }
+}
---
+++ MagicWeapon/Models/GreetingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MagicWeapon.Models
+{
+    public class GreetingViewModel
+    {
+        public string Message { get; set; }
+    }
+}
